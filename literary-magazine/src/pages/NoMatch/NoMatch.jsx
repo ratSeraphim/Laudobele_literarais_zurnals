0eb02c50cb38125d25eae1609123b317ad0a1b99
ratSeraphim <telmy2458@hotmail.lv>
2023-04-26 13:30:55 +0300

    Styled 404 page

---
+++ literary-magazine/src/pages/NoMatch/NoMatch.jsx
@@ -0,0 +1,12 @@
+import { NomatchPaper, Return } from "./style";
+
+const NoMatch = () => {
+	return (
+		<NomatchPaper>
+			No page found
+			<Return to="/">Return to the sea</Return>
+		</NomatchPaper>
+	);
+};
+
+export default NoMatch;
---
+++ literary-magazine/src/pages/NoMatch/style.js
@@ -0,0 +1,25 @@
+import { Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+
+export const NomatchPaper = styled(Paper)`
+	display: flex;
+	flex-direction: column;
+	margin: 10rem auto;
+	padding: 2rem;
+	width: 20%;
+	height: 10rem;
+`;
+
+export const Return = styled(Link)`
+	background-color: #ffa726;
+	color: #333;
+	margin: 2rem;
+	padding: 1rem;
+	border-radius: 1rem;
+	&:hover {
+		transform: translate 1px;
+		background-color: #13b491;
+		color: white;
+	}
+`;
